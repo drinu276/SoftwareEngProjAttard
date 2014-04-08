54eb1e050184300c61bfc23394c2c4daf8a47311
drinu276 <andrea.attard@gmail.com>
2014-04-08 12:35:02 +0200

    first commit

---
+++ src/softwareengattard/SoftwareEngAttard.java
@@ -0,0 +1,13 @@
+package softwareengattard;
+
+public class SoftwareEngAttard {
+
+    public static void main(String[] args) {
+
+    }
+    
+    int retNum() {
+        return 0;
+    }
+    
+}
---
+++ test/softwareengattard/SoftwareEngAttardTest.java
@@ -0,0 +1,53 @@
+package softwareengattard;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SoftwareEngAttardTest {
+    
+    public SoftwareEngAttardTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class SoftwareEngAttard.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        SoftwareEngAttard.main(args);
+    }
+
+    /**
+     * Test of retNum method, of class SoftwareEngAttard.
+     */
+    @Test
+    public void testRetNum() {
+        System.out.println("retNum");
+        SoftwareEngAttard instance = new SoftwareEngAttard();
+        int expResult = 0;
+        int result = instance.retNum();
+        assertEquals(expResult, result);
+    }
+    
+}
